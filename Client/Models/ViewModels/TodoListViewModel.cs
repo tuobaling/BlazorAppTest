041b5cacd48cf0b98c54f77e8689509d930a81de
wunhu <wunhu@DESKTOP-P6IQ2NC>
2021-03-04 20:27:06 +0800

    修完

---
+++ Client/Models/ViewModels/TodoListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppTest.Client.Models.ViewModels
+{
+    public class TodoListViewModel
+    {
+        public int listId { get; set; }
+        public string fxName { get; set; }
+    }
+
+}
